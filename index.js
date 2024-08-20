1e6a256aaef20aaed83fd155eb2a35683d36b27a
Adam D <adamdebrouwere@hotmail.com>
2024-08-20 03:11:04 +0000

    feat: working

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const res = require('express/lib/response');
 const app = express();
 const bodyParser = require('body-parser');
 const { URL } = require('url');
+const dns = require('dns')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -24,43 +25,50 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-let urlDb = {};
-let urlIdGenerator = 1;
-
-app.post('/api/shorturl', function (req, res) {
-
-  const ogURL = req.body.url;
-  const shortURL = urlIdGenerator++;
-
-  urlDb[shortURL] = ogURL;
-
+function isValidUrl(inputUrl) {
+  
   try {
-    new URL(ogURL);
+    new URL(inputUrl);
+    return true;
   } catch (error) {
     return res.json({ error: 'invalid url' });
   }
+}
 
+let urlDb = {};
+let urlIdGenerator = 1;
+const regexUrlPattern = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}(\/[^\s]*)?$/;
+
+app.post('/api/shorturl', function (req, res) {
+  const ogURL = req.body.url;
+  
+  if (!ogURL || !isValidUrl(ogURL)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+
+  const hostname = new URL(ogURL).hostname;
+  dns.lookup(hostname, function (err) {
+    if (err) {
+        return res.json({ error: 'invalid url'})
+    }
+  
+    const shortURL = urlIdGenerator++;
+    urlDb[shortURL] = ogURL;
+    
   res.json({
-    original_url: ogURL,
-    short_url: shortURL
+      original_url: ogURL,
+      short_url: shortURL
+    });
   });
 });
 
-app.get('/api/shorturl/:short_url', getOgURL, function (req, res) {
-  res.json({ error: 'invalid url'})
-}); 
-
-function getOgURL(req, res, next) {
-  const shortURL=parseInt(req.params.short_url);
-  const ogURL=urlDb[shortURL];
-
-  if(!ogURL) {
-    res.locals.error = "no short URL found"
-    return next();
-  }
-
+app.get('/api/shorturl/:short_url', function (req, res) {
+  const shortURL = parseInt(req.params.short_url);
+  const ogURL = urlDb[shortURL];
+  
   res.redirect(ogURL)
-}
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
